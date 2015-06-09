71b6af6e9b3750fdc54327b53bc09631ac48ef1d
rylon <rylon@web.de>
2015-06-09 20:48:17 +0200

    updated markdown to 1.5

--- lib_markdown.php
+++ lib_markdown.php
@@ -4,8 +4,9 @@ class lib_markdown extends \LIB\lib_php{
     public static function get_class($params = null){
         return self::class;}
     public static function php_autoload(){
-        \SYSTEM\autoload::registerFolder(dirname(__FILE__).'/lib','Michelf');
+        require_once dirname(__FILE__).'/lib/Michelf/Markdown.inc.php';
+        require_once dirname(__FILE__).'/lib/Michelf/MarkdownExtra.inc.php';
         return true;}
     public static function version(){
-        return '1.3';}
+        return '1.5';}
 }
